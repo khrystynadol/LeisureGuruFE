3fa10059e3dfcb88e97524d2cb0aff0f89a8f0f9
tufimtseva <tetiana.ufimtseva.kn.2021@lpnu.ua>
2022-09-29 23:45:10 +0300

    merged version

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,98 +1,115 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React, {useEffect, useState } from "react";
 import Header from "./Header";
-import { Home } from "./Home";
-import { Main } from "./Main";
 import { useNavigate } from "react-router-dom";
 
 
 export const Login = function() {
-    // return (<div>
-    //     <div><span >Username</span> </div> 
-      
-    //     <div><span >Password</span> </div> 
+  const[password, setPassword] = useState('')
+  const[dirtyEmail, setDirtyEmail] = useState(false)
+  const[dirtyPassword, setDirtyPassword] = useState(false)
+  const[errorEmail, setErrorEmail] = useState('Email field can`t be empty')
+  const[errorPassword, setErrorPassword] = useState('Password field can`t be empty')
+  const[formValid, setFormValid] = useState(false)
+  const [loginErrorMessage, setLoginErrorMessage] = useState();
 
-    //   </div>);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
 
-    const [errorMessages, setErrorMessages] = useState({});
-    const [isSubmitted, setIsSubmitted] = useState(false);
+  useEffect (() => {
+    if (errorEmail || errorPassword) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [errorEmail, errorPassword])
 
-    const handleSubmit = (event) => {
-        //Prevent page reload
-        event.preventDefault();
-      
-        var { uname, pass } = document.forms[0];
-      
-        // Find user login info
-        const userData = database.find((user) => user.username === uname.value);
-        console.log(userData);
-        // Compare user info
-        if (userData) {
-          if (userData.password !== pass.value) {
-            // Invalid password
-            setErrorMessages({ name: "pass", message: errors.pass });
-          } else {
-            setIsSubmitted(true);
-            navigate("/homepage");
-          }
-        } else {
-          // Username not found
-          setErrorMessages({ name: "uname", message: errors.uname });
-        }
-      };
+
+  const emailHandler = (e) => {
+    setDirtyEmail(true)
+      const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!e.target.value) {
+      setErrorEmail('Email can`t be empty')
+    } else if(!re.test(String(e.target.value).toLowerCase())) {
+        setErrorEmail('Incorrect email')
+    } else {
+      setErrorEmail("")
+    }
+  }
+
+  const passwordHandler = (e) => {
+    setDirtyPassword(true)
+    setPassword(e.target.value)
+    if (e.target.value.length < 8) {
+      setErrorPassword('Your password should have at least 8 characters')
+      if (!e.target.value) {
+        setErrorPassword('Password can`t be empty');
+      }
+    }else{
+      setErrorPassword("")
+    }
+  }
+
+  
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { email, pass } = document.forms[0];
+    // Find user login info
+    const userData = database.find((user) => user.email === email.value);
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setLoginErrorMessage("Invalid password");
+      } else {
+        navigate("/homepage");
+      }
+    } else {
+      setLoginErrorMessage("Email not found");
+    }
+  };
     
-
-    const renderForm = (
-        <div className="form">
-            <div className="form-body">
-          <form onSubmit={handleSubmit}>
-            <div className="username">
-              <label className="form__label">Username </label>
-              <input type="text" name="uname" required />
-              {/* {renderErrorMessage("uname")} */}
-            </div>
-            <div className="password">
-              <label className="form__label">Password </label>
-              <input type="password" name="pass" required />
-              {/* {renderErrorMessage("pass")} */}
-            </div>
-            
-            <div className="button-container">
-              <input type="submit" />
-            </div>
-          </form>
-          </div>
+  const renderForm = (
+    <div className="form">
+        <div className="form-body">
+      <form onSubmit={handleSubmit}>
+        <div className="username">
+          <label className="form__label">Email</label>
+          <input onChange={e => emailHandler(e)}  onBlur={e=>emailHandler(e)} type="text" name="email" required />
+          {(dirtyEmail && errorEmail) && <div style = {{color: 'red'}}>{errorEmail}</div>}
         </div>
-     );
-
+        <div className="password">
+          <label className="form__label">Password </label>
+          <input onChange={e=>passwordHandler(e)} onBlur={e=>passwordHandler(e)} type="password" name="pass" required />
+          {(dirtyPassword && errorPassword) && <div style = {{color: 'red'}}>{errorPassword}</div>}
+        </div>
+          <button disabled = {!formValid} type="submit">Login</button>
+          {(loginErrorMessage) && <div style = {{color: 'red'}}>{loginErrorMessage}</div>}
+      </form>
+      </div>
+    </div>
+  );
     
-      //<form onSubmit={handleSubmit}></form>
-      // User Login info
+  // User Login info
   const database = [
     {
-      username: "user1",
-      password: "pass1"
+      email: "jonny.hehheh.kn.2021@lpnu.ua",
+      password: "pass1234"
     },
     {
-      username: "user2",
+      email: "leisure.guru@lpnu.ua",
       password: "pass2"
     }
   ];
   
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
-  
-  
-    return(
-        <div className="Login">
-        <div className="login-form">
-        <div>{Header("Log in")} </div>
+  return(
+    <div className="Login">
+      <div className="login-form">
+        <div>{Header("Log in")}</div>
         {renderForm}
       </div>
     </div>
-    );
+  );
 }
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,72 +1,109 @@
-import { render } from "@testing-library/react";
 import Header from "./Header";
 import './style.css'
-import React, {useState,setState} from 'react';
+import React, {useState,setState, useEffect} from 'react';
 import { useNavigate } from "react-router-dom";
 
 
 export const Registration = function() {
-    // return (<div>
-    //     {/* <div><span >Gmail</span> </div> 
-    //     <div><span >Password</span> </div> 
-    //     <div><span >First name</span> </div> 
-    //     <div><span >Last name</span> </div>  */}
-    //   </div>);
-
-    // const renderForm = (
-    // //     <div className="form">
-    // //       <form>
-    // //         <div className="input-container">
-    // //           <label>Email </label>
-    // //           <input type="text" name="uname" required />
-    // //         </div>
-    // //         <div className="input-container">
-    // //           <label>Password </label>
-    // //           <input type="password" name="pass" required />
-    // //         </div>
-    // //         <div className="input-container">
-    // //           <label>First name</label>
-    // //           <input type="text" name="firstname" required />
-    // //         </div>
-    // //         <div className="input-container">
-    // //           <label>Last name</label>
-    // //           <input type="text" name="lastname" required />
-    // //         </div>
-    // //         <div className="button-container">
-    // //           <input type="submit" />
-    // //         </div>
-    // //       </form>
-    // //     </div>
-    // //  );
     const navigate = useNavigate();
-    const [firstName, setFirstName] = useState(null);
-    const [lastName, setLastName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [password,setPassword] = useState(null);
-    const [confirmPassword,setConfirmPassword] = useState(null);
+    const[firstName, setFirstName] = useState('');
+    const[errorFirstName, setErrorFirstName] = useState('Name can`t be empty')
+    const[dirtyFirstName, setDirtyFirstName] = useState(false)
 
-    const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id === "firstName"){
-            setFirstName(value);
-        }
-        if(id === "lastName"){
-            setLastName(value);
-        }
-        if(id === "email"){
-            setEmail(value);
-        }
-        if(id === "password"){
-            setPassword(value);
-        }
-        if(id === "confirmPassword"){
-            setConfirmPassword(value);
-        }
+    const[lastName, setLastName] = useState('');
+    const[errorLastName, setErrorLastName] = useState('')
+    const[dirtyLastName, setDirtyLastName] = useState(false)
 
+    const[email, setEmail] = useState('')
+    const[dirtyEmail, setDirtyEmail] = useState(false)
+    const[errorEmail, setErrorEmail] = useState('Email field can`t be empty')
+
+    const[password, setPassword] = useState('')
+    const[dirtyPassword, setDirtyPassword] = useState(false)
+    const[errorPassword, setErrorPassword] = useState('Password field can`t be empty')
+    
+    const[confirmPassword,setConfirmPassword] = useState("");
+    const[dirtyConfirmPassword, setDirtyConfirmPassword] = useState(false)
+    const[errorConfirmPassword, setErrorConfirmPassword] = useState('Confirm password field can`t be empty')
+
+    const [date, setDate] = useState('')
+    const[formValid, setFormValid] = useState(false)
+
+    useEffect(() =>{
+    if(errorEmail || errorPassword || errorFirstName || errorLastName || !date){
+      setFormValid(false)//якщо хоч один з елементів форми заповнений неправильно то форма вважається не валідною
+    }else{
+      setFormValid(true)
     }
+  }, [errorEmail, errorPassword, errorFirstName, errorLastName, date])
+
+  const emailHandler = (e) => {
+      setDirtyEmail(true)
+      setEmail(e.target.value)
+      const re =
+  /^(([^<>()[\],;:\s@\"]+([^<>()[\],;:\s@\"]+)*)|(\".+\"))@(([^<>()[\],;:\s@\"]+)+[^<>()[\],;:\s@\"]{2,})$/i;
+  if(!e.target.value){
+    setErrorEmail('Email can`t be empty')
+  } else if(!re.test(String(e.target.value).toLowerCase())){
+        setErrorEmail('Incorrect email')
+    }else{
+      setErrorEmail("")
+    }
+    
+  }
+//перевірка для форми password на довжину
+  const passwordHandler = (e) => {
+    setDirtyPassword(true)
+    setPassword(e.target.value);
+    if(e.target.value.length < 5 || e.target.value.length > 10){
+      setErrorPassword('Your password should be between 5 and 10 symbols')
+      if(!e.target.value){
+        setErrorPassword('Password can`t be empty')
+      }
+    }else{
+      setErrorPassword("")
+    }
+  }
+
+  const confirmPasswordHandler = (e) => {
+    setDirtyConfirmPassword(true)
+    setConfirmPassword(e.target.value)
+    if (e.target.value ===''){
+      setErrorConfirmPassword('Confirm password can`t be empty')
+    } else if(e.target.value !== password) {
+      setErrorConfirmPassword("This password does not match the password you entered above")
+    }else{
+      setErrorConfirmPassword("")
+    }
+  }
+
+  //функція перевірки форми name на правильність написання і відсутність неправльних знаків
+  const firstNameHandler = (e) =>{
+    setDirtyFirstName(true)
+    setFirstName(e.target.value)
+    if (e.target.value ===''){
+      setErrorFirstName('Name can`t be empty')
+    } else if(!/^[a-zA-Z]+$/.test(e.target.value)){
+      setErrorFirstName('Your name contains inapropriate symbols')
+    }else{
+      setErrorFirstName('')
+    }
+  }
+
+  //функція перевірки форми surname на правильність написання і відсутність неправльних знаків
+  const lastNameHandler = (e) => {
+    setDirtyLastName(true)
+    setLastName(e.target.value)
+    if (e.target.value ===''){
+      setErrorLastName('Surname can`t be empty')
+    } else if(!/^[a-zA-Z]+$/.test(e.target.value)){
+      setErrorLastName('Your surname contains inapropriate symbols')
+    } else {
+      setErrorLastName('')
+    }
+  }
 
     const handleSubmit  = () => {
-       // console.log(firstName,lastName,email,password,confirmPassword);
        navigate("/homepage");
     }
 
@@ -74,28 +111,38 @@ export const Registration = function() {
         <div className="form">
         <div className="form-body">
             <div className="username">
-                <label className="form__label" for="firstName">First Name </label>
-                <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" placeholder="First Name"/>
+                <label className="form__label" htmlFor="firstName">First Name </label>
+                <input className="form__input" onChange={e=>firstNameHandler(e)}  onBlur={e=>firstNameHandler(e)} type="text" value={firstName}  name="firstName" placeholder="First Name"/>
+                {(dirtyFirstName && errorFirstName) && <div style = {{color: 'red'}}>{errorFirstName}</div>}
             </div>
             <div className="lastname">
-                <label className="form__label" for="lastName">Last Name </label>
-                <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="LastName"/>
+                <label className="form__label" htmlFor="lastName">Last Name </label>
+                <input  onChange={e=>lastNameHandler(e)} onBlur={e=>lastNameHandler(e)} type="text"  name="lastName" value={lastName}  className="form__input" placeholder="LastName"/>
+                {(dirtyLastName && errorLastName) && <div style = {{color: 'red'}}>{errorLastName}</div>}
             </div>
             <div className="email">
-                <label className="form__label" for="email">Email </label>
-                <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
+                <label className="form__label" htmlFor="email">Email </label>
+                <input  onChange={e=>emailHandler(e)} value = {email} onBlur={e=>emailHandler(e)} type="email" name="email" className="form__input"   placeholder="Email"/>
+                {(dirtyEmail && errorEmail) && <div style = {{color: 'red'}}>{errorEmail}</div>}
+            </div>
+            <div>
+            <label className="form__label" htmlFor="date">Birth date </label>
+            <input type = 'date' onChange = {e=>setDate(e.target.value)} value = {date}  name = 'date' placeholder = 'Pick your birth date...'/>
             </div>
             <div className="password">
-                <label className="form__label" for="password">Password </label>
-                <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
+                <label className="form__label" htmlFor="password">Password </label>
+                <input className="form__input" onChange={e=>passwordHandler(e)} value = {password} onBlur={e=>passwordHandler(e)} type="password"  name="password"   placeholder="Password"/>
+                {(dirtyPassword && errorPassword) && <div style = {{color: 'red'}}>{errorPassword}</div>}
             </div>
-            <div className="confirm-password">
-                <label className="form__label" for="confirmPassword">Confirm Password </label>
-                <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+            <div className="password">
+                <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
+                <input className="form__input" onChange={e=>confirmPasswordHandler(e)} value = {confirmPassword} onBlur={e=>confirmPasswordHandler(e)} type="password" name="confirmPassword" placeholder="Confirm Password"/>
+                {(dirtyConfirmPassword && errorConfirmPassword) && <div style = {{color: 'red'}}>{errorConfirmPassword}</div>}
             </div>
         </div>
-        <div class="footer">
-            <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
+        <div className="footer">
+           
+            <button disabled = {!formValid} type = 'submit' onClick={()=>handleSubmit()}>Register</button>
         </div>
     </div>
               
@@ -110,3 +157,5 @@ export const Registration = function() {
     </div>
      );
 }
+
+
