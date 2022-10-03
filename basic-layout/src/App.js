9fa9b5d4c122368fa7d96d39172c1e89ef3f0ef6
RabotiahovDmytro <you@example.com>
2022-10-03 15:57:37 +0300

    created basic layout for every page

---
+++ basic-layout/src/App.js
@@ -0,0 +1,25 @@
+import './styles.css';
+import * as React from 'react';
+
+function App() {
+  return (
+    <header>
+      <div class="all-header-menu">
+        <a href = "#" class = "logo">LeisureGuru</a>
+        <div class="menu">
+          <ul>
+            
+            <form>
+              <input type="text" placeholder='Browse for places here...' class = "searchTxt"></input>
+              <input type="Submit" value="Goooo" class = "searchButton"></input>
+            </form>
+            <li><a href="#" class = "notification">Notification</a></li>
+            <li><a href="#" class = "user">User</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default App;
