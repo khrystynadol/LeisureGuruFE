2be758a9c928533fe662de5063ada6872cf1ef27
RabotiahovDmytro <you@example.com>
2022-10-23 21:03:30 +0300

    added changes that mentor mentioned

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,10 +2,13 @@ import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 import { useEffect, useState, history, location } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Link} from "react-router-dom";
+import {DeleteConfirmation} from "./DeleteConfirmation";
 
 
 export const Profile = function () {
     const[serverEror, setServerEror] = useState('');
+    const [deleteMessage, setDeleteMessage] = useState(null);
+    const [displayConfirmationModal, setDisplayConfirmationModal] = useState(false);
     const navigate = useNavigate();
     const id = 0;
     function LogOut(){
@@ -36,7 +39,7 @@ export const Profile = function () {
       navigate("/");
     }
 
-    const HandleDelete = (e) => {
+    /*const HandleDelete = (e) => {
         e.stopPropagation();
            if(window.confirm('Are sure want to delete?')) {
                 fetch('http://127.0.0.1:5000/user/' + id, {
@@ -73,10 +76,58 @@ export const Profile = function () {
            } else {
                 navigate("/profile");
            }
+     }*/
 
-           
-     }
+     const showDeleteModal = () => {
+        setDeleteMessage("Are you sure you want to delete your account on LeisureGuru?")
+        setDisplayConfirmationModal(true);
+      };
       
+
+      const hideConfirmationModal = () => {
+        setDisplayConfirmationModal(false);
+      };
+
+
+      const submitDelete = (e) => {
+        setDisplayConfirmationModal(false);
+        e.stopPropagation();
+           if(window.confirm('Are sure want to delete?')) {
+                fetch('http://127.0.0.1:5000/user/' + id, {
+                    method: 'DELETE',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                })
+                .then((response) => {
+                    if (response.status >= 200 && response.status <= 299) {
+                        alert('We now don`t have any information about you');
+                        localStorage.clear();
+                        setServerEror('')
+                        navigate("/");
+                        alert('We now don`t have any information about you');
+                        localStorage.clear();
+                    } else if (response.status == 400) {
+                        setServerEror('Bad Request')
+                    } else if (response.status == 404) {
+                        setServerEror('Not Found')
+                    } else if (response.status == 500) {
+                        setServerEror ('Internal Server Error')
+                    } else if (response.status == 502) {
+                        setServerEror('Bad Gateway')
+                    } else if (response.status == 503) {
+                        setServerEror('Service Unavailable')
+                    } else if (response.status == 503) {
+                        setServerEror ('Gateway Timeout')
+                    }else{
+                        setServerEror('Unknown error')
+                        navigate("/profile");
+                    }
+                })
+           } else {
+                navigate("/profile");
+           }
+      };
     return (
     <main>
         <div className = "Container emp-profile">
@@ -104,7 +155,7 @@ export const Profile = function () {
                     </div>
                 </div>
                 <div>
-                    <button className = "deleteButton" onClick={(e) =>HandleDelete(e)}>
+                    <button className = "deleteButton" DeleteConfirmation showModal = {displayConfirmationModal} confirmModal = {submitDelete} hideModal = {hideConfirmationModal} message = {deleteMessage}>
                         Delete my account
                     </button>
                 </div>
